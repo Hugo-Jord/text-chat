a0e428c89609b57041e999b314c4f687f8aa5e39
Hugo Jordao <hugotjordao@hotmail.com>
2022-01-02 16:28:49 +0000

    vid complete

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express(); //to run server
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+
 //setup the express server
 app.set('view engine', 'html');
 app.use(express.static('public')); //all js and css goes in this public folder
@@ -12,10 +13,20 @@ app.get('/', (req, res)  => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
+const users = {}
 io.on('connection', socket => {
-    socket.emit('chat-message', 'Hello World')
+    socket.on('new-user', username =>{
+        users[socket.id] = username;
+        socket.broadcast.emit('user-connected', username)
+    })
     socket.on('send-chat-message', message => {
-        console.log(message)
+        //emit = sends to everyone including you
+        //broadcast.emit = sends to everyone except you 
+        socket.broadcast.emit('chat-message', {message: message, username: users[socket.id]})
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
     })
 })
 
