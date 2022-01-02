4b5408e64f8a014c08152f2d81c985e1fe42a1d8
Hugo Jordao <hugotjordao@hotmail.com>
2022-01-02 15:44:20 +0000

    figuring out heroku

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
-const io = require('socket.io')(process.env.PORT)
+const express = require('express'); //create express server
+const app = express(); //to run server
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+//setup the express server
+app.set('view engine', 'html');
+app.use(express.static('public')); //all js and css goes in this public folder
+
+//redirect user to a room
+app.get('/', (req, res)  => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
 io.on('connection', socket => {
     socket.emit('chat-message', 'Hello World')
 })
+
+//make server in port 3000
+server.listen(process.env.PORT || 3000);
